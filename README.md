04503d226bfc6725e161a0c5b67245d352a6acb4
Fakhir Ali <ali.fakhir33@gmail.com>
2024-01-11 00:51:28 +0500

    first proto

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# SAM labeller
-Simple [SAM](https://github.com/facebookresearch/segment-anything) labeller.
+# NeuroCanvas
+Currently using [SAM](https://github.com/facebookresearch/segment-anything) to mask out images.
+
+
+## TODO
+- [ ] Reset button
+- [ ] Save button
+- [ ] User driven as well
+- [ ] Diffusion on the masked stuff.
+
+
+## Applications
+- semantic segmentation labelling
+- Re-generating parts of an image
+- removing objects from an image
+
+## Notes
+We can become better than adobe. If we are better at Image AI stuff.
+The UI philosophy is to make python functions for everything. 
+No globals just functions and their arguments.
+A flask api, streamlit app or gradio prototypes can simply call these functions
+accordingly. Having the python library with all the functions is more important. 
+
+## Other ideas
+**Meme maker**
+Imagine if I could segment anything from one image and paste it inside another image.
+Generate meme backgrounds for the segmented out part. lol
+
--- main.py
+++ main.py
@@ -13,19 +13,24 @@ app = Flask(__name__)
 torch.set_grad_enabled(False)
 
 device = 'cuda' if torch.cuda.is_available() else "cpu"
+#print("\n", device, "\n" )
 
 model = SamModel.from_pretrained("facebook/sam-vit-base")
 model.to(device)
 processor = SamProcessor.from_pretrained("facebook/sam-vit-base")
 
+combinedMask= None
 
-def return_mask(img, input_points):
+
+def return_mask(img, input_points, dilation=5):
     inputs = processor(img, input_points=input_points, return_tensors="pt").to(device)
     outputs = model(**inputs)
     masks = processor.image_processor.post_process_masks(outputs.pred_masks.cpu(),
                                                          inputs["original_sizes"].cpu(),
                                                          inputs["reshaped_input_sizes"].cpu())
     mask = np.array(masks[0][0][-1])
+    print(dilation, mask.shape, mask.dtype)
+    mask = Dilation(mask, dilation)
     return mask
 
 
@@ -36,20 +41,57 @@ raw_image = Image.open('static/img.png').convert("RGB")
 def index():
     return render_template('index.html', file='img.png')
 
-
-@app.route('/run_model', methods=['GET'])
-def run_model():
+#add mask feature
+@app.route('/add_segment', methods=['GET']) 
+def add_segment():
+    global combinedMask
     x = request.args.get('x')
     y = request.args.get('y')
-    mask = return_mask(raw_image, [[[x, y]]])
+    dilation = request.args.get('dilation')
+    mask = return_mask(raw_image, [[[x, y]]], int(dilation))
+
+    if combinedMask is None:
+        combinedMask = mask
+    else:
+        combinedMask = np.logical_or(combinedMask, mask)
+
     orig_img = np.array(raw_image)
-    mask = np.array(mask, dtype=np.uint8) * 1
-    mask = mask.reshape(mask.shape[0], -1, 1)
-    mask_img = np.concatenate((mask * 255, mask * 0, mask * 0), 2)
+    combinedMask_display = np.array(combinedMask, dtype=np.uint8) * 1
+    combinedMask_display = combinedMask_display.reshape(combinedMask_display.shape[0], -1, 1)
+    mask_img = np.concatenate((combinedMask_display * 255, combinedMask_display * 0, combinedMask_display * 0), 2)
     final_img = cv2.addWeighted(orig_img, 1, mask_img, 0.5, 0)
     final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)
     cv2.imwrite('static/result.png', final_img)
     return jsonify({'image_path': '/static/result.png'})
+
+
+#removing feature
+@app.route('/remove_segment', methods=['GET'])
+def remove_segment():
+    global combinedMask
+    x = request.args.get('x')
+    y = request.args.get('y')
+    mask_to_remove = return_mask(raw_image, [[[x, y]]])
+
+    combinedMask = np.logical_and(combinedMask, np.logical_not(mask_to_remove)) #remove segment from whole combineMask
+
+    orig_img = np.array(raw_image)
+    combinedMask_display = np.array(combinedMask, dtype=np.uint8) * 1
+    combinedMask_display = combinedMask_display.reshape(combinedMask_display.shape[0], -1, 1)
+    mask_img = np.concatenate((combinedMask_display * 255, combinedMask_display * 0, combinedMask_display * 0), 2)
+    final_img = cv2.addWeighted(orig_img, 1, mask_img, 0.5, 0)
+    final_img = cv2.cvtColor(final_img, cv2.COLOR_BGR2RGB)
+    cv2.imwrite('static/result.png', final_img)
+    return jsonify({'image_path': '/static/result.png'})
+
+def Dilation(mask, dilationVal):
+    if mask is not None and mask.dtype != np.uint8:
+        mask = mask.astype(np.uint8)
+    kernel = np.ones((dilationVal, dilationVal), np.uint8)
+    return cv2.dilate(mask, kernel, iterations=1) if mask is not None else None
+
+
+
 
 
 if __name__ == '__main__':
